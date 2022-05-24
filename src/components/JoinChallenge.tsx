6e882ea80028ea8f5d40fd4f0e22ceb68e09e8bb
N3652 <N3652@gitlab.labranet.jamk.fi>
2022-05-24 15:04:10 +0300

    add storage folder -> image file & add emoji loop

--- src/components/JoinChallenge.tsx
+++ src/components/JoinChallenge.tsx
@@ -1,23 +1,30 @@
 import React, { useState } from 'react';
-import PlayerIcon from '../assets/player_emojies/002-cool-5.png';
 import { ChallengeRoomJoin } from '../interfaces';
 import SettingsHomeButtons from './SettingsHomeButtons';
+import {emojiArray, getEmojiImage} from './storage/Images'
 
 function JoinChallenge() {
   const [info, setInfo] = useState<ChallengeRoomJoin>();
   const [roomCode, setRoomCode] = useState("");
   const [playerName, setPlayerName] = useState("");
-  const [playerAvatar, setAvatar] = useState("");
+  const [playerAvatar, setAvatar] = useState(0);
 
   const setStuff = () => 
     setInfo({
       roomCode: roomCode,
       userName: playerName,
-      userAvatar: "1",
+      userAvatar: playerAvatar
     })
 
-  const consolelog = () =>
-    console.log(info);
+    const avatarIndex = () =>     
+    {
+      if(emojiArray.length > playerAvatar + 1){
+      setAvatar(playerAvatar + 1);
+      }
+      else (
+        setAvatar(0)
+      )
+    }
 
   return (
     <div>
@@ -27,11 +34,10 @@ function JoinChallenge() {
       <p>Pyydä koodi pelimestarilta</p>
       <input type="text" id="playerName" placeholder='Type your name' onChange={(e) => setPlayerName(e.target.value)} value={playerName}></input>
       <p>Kuvake</p>
-      <div>
-        <img src={PlayerIcon} alt="" />
+      <div>        
+        <img onClick={avatarIndex} src={getEmojiImage(playerAvatar)} />
       </div>
       <button id="joinChallenge-btn" onClick={setStuff}>Liity haasteeseen</button>
-      <button onClick={consolelog}>Liity </button>
     </div>
   );
 }
