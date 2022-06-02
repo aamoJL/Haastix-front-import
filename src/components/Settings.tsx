a82df78cb276e39fc95122bd0ee14b5e4fb4c1ab
Petteri Kovanen <N3941@student.jamk.fi>
2022-06-02 15:42:31 +0300

    yes

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,32 +1,36 @@
-import { Modal, Typography } from '@mui/material';
-import { Box } from '@mui/system';
+import { Modal, Typography, Stack, IconButton, Switch } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close'
 import * as React from 'react';
 
 const style = {
   position: 'absolute' as 'absolute',
-  top: '50%',
+  top: '20%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: 400,
   bgcolor: 'background.paper',
   border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
 };
 
 interface Props {
   open: boolean,
-  handleClose: (close: boolean) => void
+  handleClose: () => void
 };
 const Settings = (props: Props) => {
   return (
     <div>
       <Modal open={props.open} onClose={props.handleClose}>
-        <Box sx={style}>
-          <Typography variant="h6" component="h2">
-            Settings
-          </Typography>
-        </Box>
+        <Stack sx={style}>
+          <Stack direction="row-reverse" alignItems="flex-start" justifyContent="space-between">
+            <IconButton onClick={props.handleClose}>
+              <CloseIcon/>
+            </IconButton>
+            <Typography variant="h6" component="h2">
+              Settings
+            </Typography>
+          </Stack>
+          <Switch />
+        </Stack>
       </Modal>
     </div>
   );
