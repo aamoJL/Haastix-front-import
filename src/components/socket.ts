4b27d639a37556bbffbfeddbeaae6449f67bbeff
AA2958 <aa2958@student.jamk.fi>
2022-06-14 11:16:28 +0300

    add port number to websocket endpoint

--- src/components/socket.ts
+++ src/components/socket.ts
@@ -1,7 +1,7 @@
 import socketIOClient from "socket.io-client";
 
 //const websocketEndpoint = `${process.env.REACT_APP_API_URL}`;
-const websocketEndpoint = "https://test.haastix.wimmalab.org";
+const websocketEndpoint = "https://test.haastix.wimmalab.org:8000";
 
 
 export const setConnection = (token: string) => {
