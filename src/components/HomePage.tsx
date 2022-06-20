31ee1460a2cb1157b8800bcbfdbfd304c2e0383c
N3941 <n3941@student.jamk.fi>
2022-06-20 16:44:17 +0300

    banner changes color with darkmode toggle

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,21 +1,19 @@
 import { Link } from 'react-router-dom';
 import {ReactComponent as Banner} from '../assets/banner.svg';
-import banner from '../assets/banner.svg'
 import SettingsHomeButtons from './SettingsHomeButtons';
-import { Stack, Button, Box, PaletteColor } from '@mui/material';
+import { Stack, Button, Box, Palette, } from '@mui/material';
 import { Translation } from '../translations';
 
 interface Props{
   translation: Translation,
-  themeColor: PaletteColor
+  palette: Palette
 }
 
-function HomePage({translation, themeColor} : Props) {
+function HomePage({translation, palette} : Props) {
   return (
     <Stack justifyContent="flex-end" alignItems="center" spacing={3}>
       <SettingsHomeButtons isHomePage={true}/>
-      <Box component="img" src={banner} alt="Banner" sx={{mb:6, width: 0.75, height: 0.75, maxWidth: 824 , maxHeight: 282}}/>
-      <Banner style={{color: themeColor.main}} />
+      <Banner style={{fill: palette.text.primary, color: palette.primary.main, maxWidth: "75%", maxHeight: "75%"}} />
       <Box sx={{height: 150}}/>
       <Button component={Link} to="/create" variant="contained" id="createChallenge-btn">{translation.inputs.buttons.createAChallenge}</Button>
       <Button component={Link} to="/game" variant="contained" id="joinChallenge-btn">{translation.inputs.buttons.joinAChallenge}</Button>
