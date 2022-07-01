00b07497bc29bfca87ff4af9e921b38e0ccde83d
AA2958 <aa2958@student.jamk.fi>
2022-07-01 10:25:54 +0300

    change WebSocketEndpoint to URL from variable

--- src/components/socket.ts
+++ src/components/socket.ts
@@ -1,6 +1,6 @@
 import socketIOClient from "socket.io-client";
 
-const websocketEndpoint = `${process.env.REACT_APP_API_URL}`;
+const websocketEndpoint = "https://test.haastix.wimmalab.org";
 
 export const setConnection = (token: string) => {
   try {
